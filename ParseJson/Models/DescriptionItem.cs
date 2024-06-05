690f61ff0b61388b32a58383c8ea232b7cfb3d44
salih.benli <salih.benli@YAZILIM-NB-007>
2024-06-05 13:26:58 +0300

    Add project files.

---
+++ ParseJson/Models/DescriptionItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseJson.Models
+{
+    public class DescriptionItem
+    {
+        public int Line { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ ParseJson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using ParseJson;
+using ParseJson.Models;
+
+namespace JsonParseExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "response.json");
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            ReceiptData receiptData = JsonConvert.DeserializeObject<ReceiptData>(jsonContent);
+
+            foreach (var item in receiptData.Description)
+            {
+                Console.WriteLine(item.Line + " " + item.Text);
+            }
+        }
+    }
+}
